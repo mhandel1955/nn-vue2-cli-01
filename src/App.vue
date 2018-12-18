a23299c312cdb13c7888c61d3bd9dcd2a8213d5e
mhandel1955 <mhandel1955@gmail.com>
2018-12-18 12:28:26 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+   <h1>{{ title }}</h1>
+   <p>{{ greeting() }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 
 export default {
+data() {
+return {
+title: 'Your first Vue file, wououou!!'
+}
+},
+methods: {
+greeting: function() {
+return 'Yo!, what up?!?!';
+}
+}
   name: "App",
   components: {
-    HelloWorld
+    
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
